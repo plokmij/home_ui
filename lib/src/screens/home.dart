71d8e460376b666a01023550276607048b06b565
plokmij <samfan@live.com>
2019-03-07 22:30:08 +0530

    add text below rounds

--- lib/src/screens/home.dart
+++ lib/src/screens/home.dart
@@ -4,33 +4,74 @@ import '../blocs/navigation_provider.dart';
 class Home extends StatelessWidget {
   final bloc = NavigationBloc();
 
-  Widget buildCircle(BuildContext context, double height, Widget child) {
+  Widget buildCircle(
+      BuildContext context, double height, Widget child, String place) {
     return Container(
-      margin: EdgeInsets.only(right: 10.0),
-      height: height * 0.8,
-      width: height * 0.8,
-      decoration: BoxDecoration(
-        shape: BoxShape.circle,
-        color: Colors.white,
-      ),
+      padding: EdgeInsets.symmetric(vertical: 9.0),
       child: Center(
-        child: child,
-      ),
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Container(
+            margin: EdgeInsets.only(right: 10.0, bottom: 5.0),
+            height: height * 0.78,
+            width: height * 0.78,
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              color: Colors.white,
+            ),
+            child: child,
+          ),
+          Container(
+            margin: EdgeInsets.only(right: 10.0),
+            child: Text(
+              place,
+              style: TextStyle(
+                color: Colors.white,
+              ),
+              textAlign: TextAlign.center,
+            ),
+          ),
+        ],
+      )),
     );
   }
 
   Widget build(BuildContext context) {
     final height = MediaQuery.of(context).size.height / 7;
     var circlesList = List<Widget>();
-    circlesList.add(SizedBox(width: 10.0,));
-    for (int i = 0; i < 5; i++) {
-      circlesList.add(buildCircle(
+    circlesList.add(
+      SizedBox(
+        width: 10.0,
+      ),
+    );
+
+    circlesList.add(
+        buildCircle(
+          context,
+          height,
+          Icon(
+            Icons.my_location,
+            size: 48,
+            color: Colors.black,
+          ),
+          "Nearby",
+        ),
+      );
+
+    for (int i = 0; i < 10; i++) {
+      circlesList.add(
+        buildCircle(
           context,
           height,
           Icon(
             Icons.security,
+            size: 48.0,
             color: Colors.black,
-          )));
+          ),
+          "Place $i",
+        ),
+      );
     }
     return StreamBuilder(
       stream: bloc.currentIndex,
@@ -70,24 +111,21 @@ class Home extends StatelessWidget {
               return <Widget>[
                 SliverAppBar(
                   backgroundColor: Color(0xff214489),
-                  title: Container(
-                    padding: EdgeInsets.symmetric(horizontal: 5, vertical: 5),
-                    color: Colors.white,
-                    child: Text(
-                      "TITLE",
-                      style: TextStyle(
-                          color: Colors.black, fontWeight: FontWeight.w700),
-                    ),
+                  title: Text(
+                    "TITLE",
+                    style: TextStyle(
+                        color: Colors.white, fontWeight: FontWeight.w900),
                   ),
                   expandedHeight:
                       (MediaQuery.of(context).size.height / 2) * (3 / 4),
-                  pinned: true,
+                  //pinned: true,
                   forceElevated: true,
                   flexibleSpace: FlexibleSpaceBar(
                     centerTitle: true,
                     background: Image.network(
                       "http://onehdwallpaper.com/wp-content/uploads/2015/07/Beautiful-Scenery-HD-Wallpapers-Pictures-7.jpg",
                       fit: BoxFit.cover,
+                      colorBlendMode: BlendMode.darken,
                       filterQuality: FilterQuality.low,
                     ),
                   ),
@@ -96,16 +134,19 @@ class Home extends StatelessWidget {
             },
             body: Column(
               children: <Widget>[
+                SizedBox(
+                  height: 0.3,
+                ),
                 Container(
                   margin: EdgeInsets.only(bottom: 5.0),
-                  height: MediaQuery.of(context).size.height / 6,
+                  height: MediaQuery.of(context).size.height / 5.8,
                   color: Color(0xff214489),
                   child: ListView.builder(
                     padding: EdgeInsets.all(0),
                     itemCount: circlesList.length,
                     scrollDirection: Axis.horizontal,
-                    itemBuilder: (context, index){
-                      return circlesList[index];
+                    itemBuilder: (context, index) {
+                      return Center(child: circlesList[index]);
                     },
                   ),
                 ),
